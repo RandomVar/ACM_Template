4f685d86db68d70693d2543ddeff5e0eb06031a5
RandomVar <1035909127@qq.com>
2018-09-09 10:48:02 +0800

    commit

--- Graph/最大流.cpp
+++ Graph/最大流.cpp
@@ -56,126 +56,124 @@ int max_flow(int s,int t){
 
 
 /*ISAP*/
-/*steal from csl*/
-const int maxn = " ";
-struct Edge
-{
-    int from, to, cap, flow;
-    Edge(int u, int v, int c, int f)
-        : from(u), to(v), cap(c), flow(f) {}
+const int maxn= " ";
+struct Edge{
+	int from,to,cap,flow;
+	Edge(int u,int v,int c,int f):from(u),to(v),cap(c),flow(f){}
 };
-struct ISAP
-{
-    int n, m, s, t;      //结点数，边数（包括反向弧），源点编号和汇点编号
-    vector<Edge> edges;  //边表。edges[e]和edges[e^1]互为反向弧
-    vector<int> G[maxn]; //邻接表，G[i][j]表示结点i的第j条边在e数组中的序号
-    bool vis[maxn];      //BFS使用
-    int d[maxn];         //起点到i的距离
-    int cur[maxn];       //当前弧下标
-    int p[maxn];         //可增广路上的一条弧
-    int num[maxn];       //距离标号计数
-    void init(int n)
-    {
-        this->n = n;
-        for (int i = 0; i < n; i++) G[i].clear();
-        edges.clear();
-    }
-    void AddEdge(int from, int to, int cap)
-    {
-        edges.pb(Edge(from, to, cap, 0));
-        edges.pb(Edge(to, from, 0, 0));
-        int m = edges.size();
-        G[from].pb(m - 2);
-        G[to].pb(m - 1);
-    }
-    int Augumemt()
-    {
-        int x = t, a = inf;
-        while (x != s)
-        {
-            Edge& e = edges[p[x]];
-            a = min(a, e.cap - e.flow);
-            x = edges[p[x]].from;
-        }
-        x = t;
-        while (x != s)
-        {
-            edges[p[x]].flow += a;
-            edges[p[x] ^ 1].flow -= a;
-            x = edges[p[x]].from;
-        }
-        return a;
-    }
-    void BFS()
-    {
-        mem(vis, 0);
-        mem(d, 0);
-        queue<int> q;
-        q.push(t);
-        d[t] = 0;
-        vis[t] = 1;
-        while (!q.empty())
-        {
-            int x = q.front();
-            q.pop();
-            int len = G[x].size();
-            for (int i = 0; i < len; i++)
-            {
-                Edge& e = edges[G[x][i]];
-                if (!vis[e.from] && e.cap > e.flow)
-                {
-                    vis[e.from] = 1;
-                    d[e.from] = d[x] + 1;
-                    q.push(e.from);
-                }
-            }
-        }
-    }
-    int Maxflow(int s, int t)
-    {
-        this->s = s;
-        this->t = t;
-        int flow = 0;
-        BFS();
-        mem(num, 0);
-        for (int i = 0; i < n; i++)
-            if (d[i] < inf) num[d[i]]++;
-        int x = s;
-        mem(cur, 0);
-        while (d[s] < n)
-        {
-            if (x == t)
-            {
-                flow += Augumemt();
-                x = s;
-            }
-            int ok = 0;
-            for (int i = cur[x]; i < G[x].size(); i++)
-            {
-                Edge& e = edges[G[x][i]];
-                if (e.cap > e.flow && d[x] == d[e.to] + 1)
-                {
-                    ok = 1;
-                    p[e.to] = G[x][i];
-                    cur[x] = i;
-                    x = e.to;
-                    break;
-                }
-            }
-            if (!ok) //Retreat
-            {
-                int m = n - 1;
-                for (int i = 0; i < G[x].size(); i++)
-                {
-                    Edge& e = edges[G[x][i]];
-                    if (e.cap > e.flow) m = min(m, d[e.to]);
-                }
-                if (--num[d[x]] == 0) break; //gap优化
-                num[d[x] = m + 1]++;
-                cur[x] = 0;
-                if (x != s) x = edges[p[x]].from;
-            }
-        }
-        return flow;
-    }
+struct ISAP{
+	int n,m,s,t;
+	vector<Edge> edges;
+	vector<int > G[maxn];
+	bool vis[maxn];
+	int d[maxn],cur[maxn];
+	int p[maxn],num[maxn];
+	void init(int n)
+	{
+		this ->n=n;
+		for(int i=0;i<=n;i++)
+		 	G[i].clear();
+		edges.clear();
+		memset(d,0x3f3f3f3f,sizeof(d));
+	}
+	void addedge(int from,int to,int cap)
+	{
+		edges.push_back(Edge(from,to,cap,0));
+		edges.push_back(Edge(to,from,0,0));
+		m=edges.size();
+		G[from].push_back(m-2);
+		G[to].push_back(m-1);
+	}
+	bool BFS()
+	{
+		memset(vis,false,sizeof(vis));
+		queue<int >q;
+		q.push(t);
+		d[t]=0;
+		vis[t]=true;
+		while(!q.empty())
+		{
+			int u=q.front();
+			q.pop();
+			for(int i=0;i<G[u].size();i++)
+			{
+				Edge &e=edges[G[u][i]^1];
+				if(!vis[e.from]&&e.cap>e.flow)
+				{
+					vis[e.from]=true;
+					d[e.from]=d[u]+1;
+					q.push(e.from);
+				}
+			}
+		}
+		return vis[s];
+	}
+	int Augment()
+	{
+		int flow=inf;
+		for(int u=t;u!=s;u=edges[p[u]].from)
+		{
+			Edge &e=edges[p[u]];
+			flow=min(flow,e.cap-e.flow);
+		}
+		for(int u=t;u!=s;u=edges[p[u]].from)
+		{
+			edges[p[u]].flow+=flow;
+			edges[p[u]^1].flow-=flow;
+		}
+		return flow;
+	} 
+	int Maxflow(int s,int t)//cal
+	{
+		this->s=s;
+		this ->t=t;
+		int flow=0;
+		BFS();
+		if(d[s]>=n)
+			return 0;
+		memset(num,0,sizeof(num));
+		for(int i=0;i<n;i++)
+			if(d[i]<INF)
+				num[d[i]]++;
+		int u=s;
+		memset(cur,0,sizeof(cur));
+		while(d[s]<n)
+		{
+			if(u==t)
+			{
+				flow+=Augment();
+				u=s;
+			}
+			int ok=0;
+			for(int i=cur[u];i<G[u].size();i++)
+			{
+				Edge &e=edges[G[u][i]];
+				if(e.cap>e.flow && d[u]==d[e.to]+1)
+				{
+					ok=1;
+					p[e.to]=G[u][i];
+					cur[u]=i;
+					u=e.to;
+					break;
+				}
+			}
+			if(!ok)
+			{
+				int m=n-1;
+				for(int i=0;i<G[u].size();i++)
+				{
+					Edge &e=edges[G[u][i]];
+					if(e.cap>e.flow)
+						m=min(m,d[e.to]);
+				}
+				if(--num[d[u]]==0)
+					break;
+				++num[d[u]=m+1];
+				cur[u]=0;
+				if(u!=s)
+					u=edges[p[u]].from;
+			}
+		}
+		return flow;
+	}
 };
---
+++ Graph/第k短路.cpp
@@ -0,0 +1,107 @@
+const int MAXM= "" ;
+const int MAXN= "" ;
+struct node
+{
+    int v, w, next;
+}edge[MAXM], revedge[MAXM];
+struct A
+{
+    ll f, g, v;
+    bool operator <(const A a)const {
+        if(a.f == f) return a.g < g;
+        return a.f < f;
+    }
+};
+int e, vis[MAXN], q[MAXM * 5];
+ll d[MAXN];
+int head[MAXN], revhead[MAXN];
+int n, m, s, t, k;
+void init()
+{
+    e = 0;
+    memset(head, -1, sizeof(head));
+    memset(revhead, -1, sizeof(revhead));
+}
+void insert(int x, int y, int w)//插入边
+{
+    edge[e].v = y;
+    edge[e].w = w;
+    edge[e].next = head[x];
+    head[x] = e;
+    revedge[e].v = x;
+    revedge[e].w = w;
+    revedge[e].next =revhead[y];
+    revhead[y] = e++;
+}
+void spfa(int src)
+{
+    for(int i = 1; i <= n; i++) d[i] = INF;
+    memset(vis, 0, sizeof(vis));
+    vis[src] = 0;
+    int h = 0, t = 1;
+    q[0] = src;
+    d[src] = 0;
+    while(h < t)
+    {
+        int u = q[h++];
+        vis[u] = 0;
+        for(int i = revhead[u] ; i != -1; i = revedge[i].next)
+        {
+            int v = revedge[i].v;
+            int w = revedge[i].w;
+            if(d[v] > d[u] + w)
+            {
+                d[v] = d[u] + w;
+                if(!vis[v])
+                {
+                    q[t++] = v;
+                    vis[v] = 1;
+                }
+            }
+        }
+    }
+}
+ll Astar(int src, int des)
+{
+    int cnt = 0;
+    priority_queue<A>Q;
+    if(src == des) k++;
+    if(d[src] == INF) return -1;
+    A t, tt;
+    t.v = src, t.g = 0, t.f = t.g + d[src];
+    Q.push(t);
+    while(!Q.empty())
+    {
+        tt = Q.top();
+        Q.pop();
+        if(tt.v == des)
+        {
+            cnt++;
+            if(cnt == k) 
+                return tt.g;
+        }
+        for(int i = head[tt.v]; i != -1; i = edge[i].next)
+        {
+            t.v = edge[i].v;
+            t.g = tt.g + edge[i].w;
+            t.f = t.g + d[t.v];
+            Q.push(t);
+        }
+    }
+    return -1;
+}
+int main()
+{
+        init();
+        scanf("%d%d%d", &s, &t, &k);
+        for(int i = 1; i <= m; i++)
+        {
+            int x,y,w;
+            scanf("%d%d%d", &x, &y, &w);
+            insert(x, y, w);
+        }
+        spfa(t);
+        ll ans=Astar(s,t);
+    }
+    return 0;
+}
